9a64433d81102c6852aab169fe636eb02d560ab0
Yuvashree-22 <yuvashreesuji001@gmail.com>
2025-08-06 13:51:21 +0530

    Fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -346,4 +346,4 @@ def boss_logout():
 
 # --- Run ---
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
